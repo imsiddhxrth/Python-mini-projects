a4f709c91e1babd742f5e83fd29f80e967849918
imsiddhxrth <siddharthpratap21@gmail.com>
2026-05-31 00:38:48 +0530

    Lottery System v1.1 - Generate unique ticket numbers for each participant - Display ticket numbers for all 3 winners - Shuffle prizes for truly random prize distribution

--- Raffle Prize Picker/picker.py
+++ Raffle Prize Picker/picker.py
@@ -1,22 +1,26 @@
 import random
 print("Welcome to the Raffle!")
-name = []
+names = []
 count = int(input("How many people are entering? "))
 for i in range(count):
     if count < 3:
         quit("Sorry, you need at least 3 people to enter the raffle.")
     else:
         print("Kindly enter your name (or type 'done' to finish): ")
-        name.append(input(": ").lower())
+        names.append(input(": ").lower())
 prize = []
 for i in range(3):
     prize.append(input("Enter 3 prizes: "))
-
+tickets = {name: random.randint(1, 100) for name in names}
+print(tickets)
+print(input("The raffle is now closed. The winners are being picked...(press enter to continue): "))
+random.shuffle(prize)
 for i in range(3):
-    winner = random.choice(name)
-    name.remove(winner)
+    winners = random.choice(names)
+    names.remove(winners)
+    print(f'{winners} Your lottery ticket number is {tickets[winners]}')
     if i == 2:
-        print (f'{winner} You have won {prize[i]} the Grand prize!')
+        print (f'{winners} You have won {prize[i]} the Grand prize!')
     elif i == 1:
-        print (f'{winner} You have won {prize[i]} the Second prize!')
-    else:        print (f'{winner} You have won {prize[i]} the Third prize!')
+        print (f'{winners} You have won {prize[i]} the Second prize!')
+    else:        print (f'{winners} You have won {prize[i]} the Third prize!')
