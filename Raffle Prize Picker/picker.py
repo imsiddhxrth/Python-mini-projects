a80487a9cf59356e6e2e92b43ce25b86038e20cb
imsiddhxrth <siddharthpratap21@gmail.com>
2026-05-31 01:32:34 +0530

    Prize Distribution v1.2 - Fixed prize pool of 1000 - 50% / 30% / 20% payout distribution - Prize amounts displayed alongside winner announcements

--- Raffle Prize Picker/picker.py
+++ Raffle Prize Picker/picker.py
@@ -8,19 +8,19 @@ for i in range(count):
     else:
         print("Kindly enter your name (or type 'done' to finish): ")
         names.append(input(": ").lower())
-prize = []
-for i in range(3):
-    prize.append(input("Enter 3 prizes: "))
+prize = 1000
+prize1 = prize * 0.5
+prize2 = prize * 0.3
+prize3 = prize * 0.2
+prize = [prize1, prize2, prize3]
 tickets = {name: random.randint(1, 100) for name in names}
 print(tickets)
 print(input("The raffle is now closed. The winners are being picked...(press enter to continue): "))
-random.shuffle(prize)
 for i in range(3):
     winners = random.choice(names)
     names.remove(winners)
-    print(f'{winners} Your lottery ticket number is {tickets[winners]}')
-    if i == 2:
-        print (f'{winners} You have won {prize[i]} the Grand prize!')
+    if i == 0:
+        print (f'{winners} You have won {prize[0]} the Grand prize(Your ticket number is {tickets[winners]}!)')
     elif i == 1:
-        print (f'{winners} You have won {prize[i]} the Second prize!')
-    else:        print (f'{winners} You have won {prize[i]} the Third prize!')
+        print (f'{winners} You have won {prize[1]} the Second prize(Your ticket number is {tickets[winners]}!)')
+    else:        print (f'{winners} You have won {prize[2]} the Third prize(Your ticket number is {tickets[winners]}!)')
