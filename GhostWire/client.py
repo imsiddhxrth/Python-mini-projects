a2c95f01258447cfd08c6b47981ad152b3d27d01
imsiddhxrth <siddharthpratap21@gmail.com>
2026-06-03 15:13:32 +0530

    GhostWire v2.1 - Multi-Client Server - Added threading to handle multiple clients simultaneously - Server creates new thread for each client connection - Client keeps sending messages in a loop until exit - Active connection count displayed on new connection

--- GhostWire/client.py
+++ GhostWire/client.py
@@ -2,8 +2,12 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 5000))
-print('Connected to GhostWire')
+print('Connected to GhostWire!')
 
-msg = input('Enter message: ')
-client.send(msg.encode())
+while True:
+    msg = input('Enter message: ')
+    if msg.lower() == 'exit':
+        break
+    client.send(msg.encode())
+
 client.close()
--- GhostWire/server.py
+++ GhostWire/server.py
@@ -1,13 +1,26 @@
 import socket
+import threading
+
+def handle_client(conn, addr):
+    print(f'New connection from {addr}')
+    while True:
+        try:
+            msg = conn.recv(1024).decode()
+            if not msg:
+                break
+            print(f'Received from {addr}: {msg}')
+        except:
+            break
+    conn.close()
+    print(f'Connection closed from {addr}')
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 5000))
 server.listen()
-print('GhostWire Server listening')
+print('GhostWire Server listening on port 5000...')
 
-conn, addr = server.accept()
-print(f'Connected by {addr}')
-
-msg = conn.recv(1024).decode()
-print(f'Received: {msg}')
-conn.close()
+while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
+    print(f'Active connections: {threading.active_count() - 1}')
